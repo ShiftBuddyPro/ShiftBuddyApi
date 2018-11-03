204419cf796596467ce281636e8706d29f54bb9e
Usman <u.ghani2013@gmail.com>
2018-11-02 22:40:03 -0500

    Add route for nested inventory items

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
       resources :shifts, except: %i[create]
       resources :cash_drops, except: %i[create]
       resources :checks, except: %i[create]
+      resources :inventory_items, except: %i[create]
       resources :notes, except: %i[create]
       resources :paid_outs, except: %i[create]
-      resources :inventory_items, except: %i[create]
       resources :managers do
         scope module: :managers do
           resources :employees, only: %i[index create] do
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
                 scope module: :shifts do
                   resources :cash_drops, only: %i[index create]
                   resources :checks, only: %i[index create]
+                  resources :inventory_items, only: %i[index create]
                 end
               end
             end
