69b328896e2cb0c13cc98cc071513c0289ac2b8d
Usman <u.ghani2013@gmail.com>
2018-12-24 08:27:57 -0600

    Add shift model test

--- test/models/shift_test.rb
+++ test/models/shift_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ShiftTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  attr_reader :manager, :employee, :tracked_item
+
+  setup do
+    @manager = create :manager
+    @employee = create :employee, manager_id: manager.id
+    @tracked_item = create :tracked_item, manager_id: manager.id
+  end
+
+  test 'should set inventory items based on manager tracked items' do
+    shift = create :shift, employee_id: employee.id
+    assert shift.inventory_items.first
+    item = shift.inventory_items.first
+    assert_equal tracked_item.name, item.name
+    assert_equal 0, item.start_amount
+  end
 end
