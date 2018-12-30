7978c2c64cf590d767b7277890ad62c7f317f83a
Usman <u.ghani2013@gmail.com>
2018-12-29 21:20:41 -0600

    Change chage_sheets to change_sheet and make random number 1 digit

--- test/factories/change_sheets.rb
+++ test/factories/change_sheets.rb
@@ -1,21 +1,21 @@
 FactoryBot.define do
-  factory :change_sheets do
-    start_pennies { Faker::Number.number(2) }
-    start_nickels { Faker::Number.number(2) }
-    start_dimes { Faker::Number.number(2) }
-    start_quarters { Faker::Number.number(2) }
-    start_ones { Faker::Number.number(2) }
-    start_fives { Faker::Number.number(2) }
-    start_tens { Faker::Number.number(2) }
-    start_twenties { Faker::Number.number(2) }
-    end_pennies { Faker::Number.number(2) }
-    end_nickels { Faker::Number.number(2) }
-    end_dimes { Faker::Number.number(2) }
-    end_quarters { Faker::Number.number(2) }
-    end_ones { Faker::Number.number(2) }
-    end_fives { Faker::Number.number(2) }
-    end_tens { Faker::Number.number(2) }
-    end_twenties { Faker::Number.number(2) }
+  factory :change_sheet do
+    start_pennies { Faker::Number.number(1) }
+    start_nickels { Faker::Number.number(1) }
+    start_dimes { Faker::Number.number(1) }
+    start_quarters { Faker::Number.number(1) }
+    start_ones { Faker::Number.number(1) }
+    start_fives { Faker::Number.number(1) }
+    start_tens { Faker::Number.number(1) }
+    start_twenties { Faker::Number.number(1) }
+    end_pennies { Faker::Number.number(1) }
+    end_nickels { Faker::Number.number(1) }
+    end_dimes { Faker::Number.number(1) }
+    end_quarters { Faker::Number.number(1) }
+    end_ones { Faker::Number.number(1) }
+    end_fives { Faker::Number.number(1) }
+    end_tens { Faker::Number.number(1) }
+    end_twenties { Faker::Number.number(1) }
     shift
   end
 end
