cfe2054a93336369150764c815e992c4d9a84736
Usman <u.ghani2013@gmail.com>
2019-01-31 23:57:44 -0600

    Add build

---
+++ public/precache-manifest.fbf8b8555026269d2831c38ba705a125.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca5c530d18eb74db6152",
+    "url": "/static/js/main.ca5c530d.chunk.js"
+  },
+  {
+    "revision": "459778cc000f2814aeb3",
+    "url": "/static/js/1.459778cc.chunk.js"
+  },
+  {
+    "revision": "ca5c530d18eb74db6152",
+    "url": "/static/css/main.166cf422.chunk.css"
+  },
+  {
+    "revision": "459778cc000f2814aeb3",
+    "url": "/static/css/1.0756d15c.chunk.css"
+  },
+  {
+    "revision": "f514e8d1899a61e5138d3464417fc06a",
+    "url": "/index.html"
+  }
+];
