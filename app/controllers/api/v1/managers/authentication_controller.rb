25368924dc785f1289420cab47aad1f09c5a4aaf
Usman <u.ghani2013@gmail.com>
2019-05-05 15:07:08 -0500

    Return manager in responsegs

--- app/controllers/api/v1/managers/authentication_controller.rb
+++ app/controllers/api/v1/managers/authentication_controller.rb
@@ -3,7 +3,9 @@ class Api::V1::Managers::AuthenticationController < ApplicationController
     command = AuthenticateManager.call(params[:email], params[:password])
 
     if command.success?
-      render json: { auth_token: command.result }
+      render({
+        json: Manager.find_by(email: params[:email]).as_json.merge({ auth_token: command.result })
+      })
     else
       render json: { error: command.errors }, status: :unauthorized
     end
