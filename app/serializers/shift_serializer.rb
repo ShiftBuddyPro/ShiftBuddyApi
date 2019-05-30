3e2875d845ea7383022da7862b5f3f7ab978a9f3
Usman <u.ghani2013@gmail.com>
2019-05-29 21:50:30 -0500

    Add employee_id and employee_name to shift serializer

--- app/serializers/shift_serializer.rb
+++ app/serializers/shift_serializer.rb
@@ -2,6 +2,14 @@ class ShiftSerializer < ApplicationSerializer
   attribute :id
   attribute :created_at
 
+  attribute :employee_id do |shift|
+    shift.employee.id
+  end
+
+  attribute :employee_name do |shift|
+    shift.employee.name
+  end
+
   has_many :notes
   has_many :checks
   has_many :cash_drops
