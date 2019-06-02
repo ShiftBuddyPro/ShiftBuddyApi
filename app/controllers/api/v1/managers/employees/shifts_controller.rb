4ddb3a188ede3ae12516dee573dfadb4ee40956c
Usman <u.ghani2013@gmail.com>
2019-06-02 15:38:39 -0500

    Update employees/shifts controllergs

--- app/controllers/api/v1/managers/employees/shifts_controller.rb
+++ app/controllers/api/v1/managers/employees/shifts_controller.rb
@@ -1,12 +1,16 @@
 class Api::V1::Managers::Employees::ShiftsController < Api::EmployeeApplicationController
-  before_action -> { render_unauthorized unless current_user_is?(manager) || current_user_is?(employee) }
+  before_action lambda {
+                  unless current_user_is?(manager) || current_user_is?(employee)
+                    render_unauthorized
+                  end
+                }
   attr_accessor :employee_id
 
   # GET /shifts
   def index
     shifts = Employee.find(params[:employee_id]).shifts
-      render json: shifts.all,
-      each_serializer: EmployeeSerializer
+    render json: ShiftsSerializer.new(shifts.all).serialized_json,
+           each_serializer: EmployeeSerializer
   end
 
   # POST /shifts
@@ -20,7 +24,7 @@ class Api::V1::Managers::Employees::ShiftsController < Api::EmployeeApplicationC
     end
   end
 
-private
+  private
 
   def manager
     @manager ||= Manager.find params[:manager_id]
