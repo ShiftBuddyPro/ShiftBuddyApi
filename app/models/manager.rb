87ba3da9b24cec0822afcd38f0acd7a205395ea4
Usman <u.ghani2013@gmail.com>
2019-06-08 12:47:05 -0500

    Add business model and add has_one to manager

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,5 +1,6 @@
 class Manager < ApplicationRecord
   has_secure_password
+  has_one :business, dependent: :destroy
   has_many :employees, dependent: :destroy
   has_many :tracked_items, dependent: :destroy
   has_many :shifts, through: :employees
