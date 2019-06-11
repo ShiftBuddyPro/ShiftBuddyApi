1d9ba3e766237ec1486b16574566cb0b0ceb37ab
Usman <u.ghani2013@gmail.com>
2019-06-11 10:45:15 -0500

    Run build

---
+++ client/build/precache-manifest.86543cc75e395d9e3758e22f1fccecb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7d7813d192f96d85228a",
+    "url": "/static/js/main.7d7813d1.chunk.js"
+  },
+  {
+    "revision": "e5bb048f2d0afaac839b",
+    "url": "/static/js/1.e5bb048f.chunk.js"
+  },
+  {
+    "revision": "7d7813d192f96d85228a",
+    "url": "/static/css/main.dfe6068d.chunk.css"
+  },
+  {
+    "revision": "e5bb048f2d0afaac839b",
+    "url": "/static/css/1.4ae84fae.chunk.css"
+  },
+  {
+    "revision": "c1a57517f22ce65bbd65e90b1d12de61",
+    "url": "/index.html"
+  }
+];
