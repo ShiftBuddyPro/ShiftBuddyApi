705f0db5cc2ba6e4f0911dcd84221c8d9d5e3770
Usman <u.ghani2013@gmail.com>
2019-06-22 20:08:28 -0500

    Add appColors and constants directory

---
+++ client/src/constants/appColors.js
@@ -0,0 +1,55 @@
+export default {
+  grey: {
+    lighter: '#f5f5f5',
+    light: '#DDD',
+    regular: 'silver',
+    dark: '#303030',
+  },
+
+  black: '2b2b2b',
+
+  white: 'white',
+
+  // orange: {
+  //   regular: '#FDA50f'
+  // },
+
+  background: {
+    // regular: '#f5f7fa'
+    regular: '#fbfbfb',
+  },
+
+  primary1: {
+    light: '#71B3FF',
+    regular: '#4B9EFF',
+    dark: '#1180FF',
+  },
+
+  primary: {
+    light: '#3498db',
+    regular: '#2980b9',
+  },
+
+  secondary: {
+    regular: '#ff8605',
+  },
+
+  success: {
+    regular: 'green',
+  },
+
+  yellow: '#FCD116',
+  darkerOrange: '#ff8547',
+  darkOrange: '#ff9447',
+  orange: '#FDA50F',
+  lightOrange: '#ffc04c',
+  lighterOrange: '#ffd27f',
+  blue: '#6699cc',
+  darkBlue: '#4477b2',
+  textGrey: '#222',
+  darkGrey: '#3f3f3f',
+  lightGrey: '#d3d3d3',
+  lighterGrey: '#E8E8E8',
+  lightestGrey: '#F5F5F5',
+  darkRed: '#c23b22',
+};
