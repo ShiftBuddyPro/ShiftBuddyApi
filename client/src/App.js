5c8293a37816b269ef511acd38782efb8c8ee172
Usman <u.ghani2013@gmail.com>
2019-06-22 20:13:17 -0500

    Make new Home bee the route for landing

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SiteNavbar from "./components/utilities/SiteNavbar";
-import Landing from "./components/public/Landing";
+import Landing from "./screens/Home";
 import Login from "./components/public/Login";
 import Dashboard from "./components/manager/Dashboard";
 import Employee from "./components/manager/Employee";
@@ -18,49 +18,46 @@ import Contact from "./components/public/Contact";
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Router>
-          <div>
-            <SiteNavbar />
-            <Switch>
-              <Route exact path="/" component={Landing} />
-            </Switch>
-            <Switch>
-              <Route exact path="/dashboard" component={Dashboard} />
-            </Switch>
-            <Switch>
-              <Route exact path="/login" component={Login} />
-            </Switch>
-            <Switch>
-              <Route exact path="/shifts" component={Shifts} />
-            </Switch>
-            <Switch>
-              <Route exact path="/shift" component={Shift} />
-            </Switch>
-            <Switch>
-              <Route exact path="/employee" component={Employee} />
-            </Switch>
-            <Switch>
-              <Route exact path="/employees" component={Employees} />
-            </Switch>
-            <Switch>
-              <Route exact path="/employeesnew" component={EmployeesNew} />
-            </Switch>
-            <Switch>
-              <Route exact path="/settings" component={Settings} />
-            </Switch>
-            <Switch>
-              <Route exact path="/inventory-items" component={InventoryItems} />
-            </Switch>
-            <Switch>
-              <Route exact path="/privacy-policy" component={PrivacyPolicy} />
-            </Switch>
-            <Switch>
-              <Route exact path="/contact" component={Contact} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <Router>
+        <>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+          </Switch>
+          <Switch>
+            <Route exact path="/dashboard" component={Dashboard} />
+          </Switch>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+          </Switch>
+          <Switch>
+            <Route exact path="/shifts" component={Shifts} />
+          </Switch>
+          <Switch>
+            <Route exact path="/shift" component={Shift} />
+          </Switch>
+          <Switch>
+            <Route exact path="/employee" component={Employee} />
+          </Switch>
+          <Switch>
+            <Route exact path="/employees" component={Employees} />
+          </Switch>
+          <Switch>
+            <Route exact path="/employeesnew" component={EmployeesNew} />
+          </Switch>
+          <Switch>
+            <Route exact path="/settings" component={Settings} />
+          </Switch>
+          <Switch>
+            <Route exact path="/inventory-items" component={InventoryItems} />
+          </Switch>
+          <Switch>
+            <Route exact path="/privacy-policy" component={PrivacyPolicy} />
+          </Switch>
+          <Switch>
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
+        </>
+      </Router>
     );
   }
 }
