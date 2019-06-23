3181b5364c0c0f4f5372da921689c237a3b8dce1
Usman <u.ghani2013@gmail.com>
2019-06-22 20:52:05 -0500

    Run build script

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4cf7ebbc9f54abf96e61eb3e282dcdb8.js"
+  "/precache-manifest.8ee6e6b2a0ef9dab0635a7c08ea3139d.js"
 );
 
 workbox.clientsClaim();
