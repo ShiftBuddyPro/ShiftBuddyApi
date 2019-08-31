b81dd1de43db41c5b3517a36ae569c3757b13a9e
Usman <u.ghani2013@gmail.com>
2019-08-31 01:43:41 -0500

    Update about page

--- client/src/screens/About/index.jsx
+++ client/src/screens/About/index.jsx
@@ -24,6 +24,29 @@ const About = props => {
           <IPhoneMockup width={300} type={1} />
           <IPhoneMockup width={300} type={3} />
         </div>
+        <Card>
+          ShiftBuddyPro is a revolutionary new tool intended to overhaul the way
+          that businesses handle shift reports. Traditional paper shift reports
+          are wasteful, don't do calculations for you and can be impossible to
+          keep track of. With ShiftBuddyPro all of those problems are a thing of
+          the past. ShiftBuddyPro will seamlessly take the place of your current
+          employee shift reporting process and put all the data you need to run
+          your business in the palm of your hands. You'll recieve real time
+          updates to your phone, tablet, or computer whenever any activity
+          occurs in your business from a check being written to change being
+          taken out of the safe. Not only this but you'll be able access any
+          shift report at any time as well as have access to trends based on
+          your reports thanks to our state of the art Data Visualization Engine.
+          Contact{" "}
+          <a
+            style={{ color: "blue" }}
+            href="mailto:usman@shiftbuddypro.com"
+            target="_blank"
+          >
+            usman@shiftbuddypro.com
+          </a>{" "}
+          for pricing and to take your business into the future.
+        </Card>
       </Body>
     </Container>
   );
@@ -53,4 +76,13 @@ const Body = styled(UI.Column)`
   flex: 1;
 `;
 
+const Card = styled.p`
+  font-weight: bold;
+  background-color: #fafafa;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 1em;
+  margin-left: 2em;
+  margin-right: 2em;
+  padding: 1em;
+`;
 export default About;
